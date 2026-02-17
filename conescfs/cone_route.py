eaf538b3383fcdbdb85da9baf532b6f48e03126e
Will White <Will.White@arup.com>
2026-02-17 19:51:31 +0000

    updated some of the cone and tt scf stuff

--- conescfs/cone_route.py
+++ conescfs/cone_route.py
@@ -1,8 +1,9 @@
 import numpy as np
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, url_for
 # local
 from conescfs.coneplotter import cone_scfs_plot
 from conescfs.scfprocess import cone_scf_single, cone_scf_sweep
+from conescfs.thktransitionscfs import calc_scf_thickness_transition
 
 app = Flask(__name__)
 
@@ -18,35 +19,45 @@ def cone_route():
         data = request.get_json()
 
         # Convert numeric fields automatically
-        numeric_fields = ["radius_tubular", "thickness_tubular", "thickness_cone", "alpha"]
-        for field in numeric_fields:
-            if field in data and data[field] != "":
-                data[field] = float(data[field])
+        for k, v in data.items():
+            try:
+                data[k] = float(v)
+            except (TypeError, ValueError):  # Leave non-numeric values unchanged
+                pass
 
-        print("Converted data:", data)
-
-        # Extract values
+        # Extract form values
+        # cone geom
         radius_tubular = data.get("radius_tubular")
         thickness_tubular = data.get("thickness_tubular")
         thickness_cone = data.get("thickness_cone")
         alpha = data.get("alpha")
         junction_type = data.get("cone_junction")  # "small" or "large"
         cone_x_axis_vary = data.get("cone_x_axis_vary")
+        # thickness transition stuff
+        transition_side = data.get("transition_side")
+        scf_weld_type = data.get("scf_weld_type")
+        weld_width = data.get('weld_width')
+        # scf taper ratio only allows a dropdown box of allowable values
+        scf_taper_ratio = float(data["scf_taper_ratio"]) if data.get("scf_taper_ratio") in (3., 4., 5., 6.) else None
+        delta_m = data.get('delta_m')
+        delta_0 = data.get('delta_0')
+        scf_inclusion = data.get('scf_inclusion')
 
-        numeric_inputs = {
+        cone_scf_numeric_fields = {
             "radius_tubular": radius_tubular,
             "thickness_tubular": thickness_tubular,
             "thickness_cone": thickness_cone,
             "alpha": alpha
         }
 
-        base_val = numeric_inputs[cone_x_axis_vary]
+        base_val = cone_scf_numeric_fields[cone_x_axis_vary]
         x_arr = np.linspace(base_val * 0.5, base_val * 1.5, 21)
 
+        # cone SCF processing-------------------------------------------------------------------------------------------
         single_results = cone_scf_single(radius_tubular, thickness_tubular, thickness_cone, alpha, junction_type)
-        sweep_results = cone_scf_sweep(junction_type, cone_x_axis_vary, x_arr, numeric_inputs)
+        sweep_results = cone_scf_sweep(junction_type, cone_x_axis_vary, x_arr, cone_scf_numeric_fields)
 
-        # single (one off) SCF results----------------------------------------------------------
+        # single (one off) SCF results---------------------
         # --- Section 3 ---
         scf_tube_in = single_results["sect3"]["tube_in"]
         scf_cone_in = single_results["sect3"]["cone_in"]
@@ -59,7 +70,7 @@ def cone_route():
         scf_tube_out_appf = single_results["appf17"]["tube_out"]
         scf_cone_out_appf = single_results["appf17"]["cone_out"]
 
-        # sweep results SCF curves----------------------------------------------------------
+        # sweep results SCF curves-----------------------
         # --- Section 3 curves ---
         scf_tube_ins = sweep_results["sect3"]["tube_in"]
         scf_cone_ins = sweep_results["sect3"]["cone_in"]
@@ -76,6 +87,45 @@ def cone_route():
         conescfs_plot_json_in = cone_scfs_plot(x_arr.tolist(), scf_tube_ins, scf_tube_in_appfs, scf_cone_ins, scf_cone_in_appfs, junction_type, cone_x_axis_vary, "INSIDE")
         conescfs_plot_json_out = cone_scfs_plot(x_arr.tolist(), scf_tube_outs, scf_tube_out_appfs, scf_cone_outs, scf_cone_out_appfs, junction_type, cone_x_axis_vary, "OUTSIDE")
 
+        # thickness transition SCF processing--------------------------------------------------------------------------
+        # scfs inside and outside
+        thk_diff = abs(thickness_tubular - thickness_cone)
+
+        # thicker tube than cone
+        if thickness_tubular >= thickness_cone:
+
+            thickness_thick_member, thickness_thin_member = thickness_tubular, thickness_cone
+
+            if transition_side == "inside":  # i.e. outer diameter is flush
+                diameter_thick_member = radius_tubular * 2
+                diameter_thin_member = radius_tubular * 2
+            elif transition_side == "outside":
+                diameter_thick_member = radius_tubular * 2
+                diameter_thin_member = radius_tubular * 2 - 2 * thk_diff
+
+
+        # thicker cone than tube
+        elif thickness_cone > thickness_tubular:
+
+            thickness_thick_member, thickness_thin_member = thickness_cone, thickness_tubular
+
+            if transition_side == "inside":  # i.e. outer diameter is flush
+                diameter_thick_member = radius_tubular * 2
+                diameter_thin_member = radius_tubular * 2
+
+            elif transition_side == "outside":
+                diameter_thick_member = radius_tubular * 2 + 2 * thk_diff
+                diameter_thin_member = radius_tubular * 2
+
+        # calculate weld width
+        scf_inside_tt, scf_outside_tt = calc_scf_thickness_transition(diameter_thick_member, diameter_thin_member,
+                                               thickness_thick_member, thickness_thin_member, weld_width,
+                                               delta_m, delta_0, scf_taper_ratio,  scf_weld_type, transition_side)
+
+
+        # images
+        f_cone_img, f_tt_img = get_cone_and_tt_imgs(junction_type, transition_side, scf_weld_type, thickness_transition=abs(thickness_cone-thickness_tubular))
+
         # Return results as JSON
         return jsonify({"status": "success",
                         "cone_junction": junction_type,
@@ -89,9 +139,17 @@ def cone_route():
                         "scf_cone_in_appf": scf_cone_in_appf,
                         "scf_tube_out_appf": scf_tube_out_appf,
                         "scf_cone_out_appf": scf_cone_out_appf,
+                        # thickness transition
+                        "scf_inside_tt":  scf_inside_tt,
+                        "scf_outside_tt": scf_outside_tt,
+                        # images
+                        "cone_scf_img": url_for("static", filename=f_cone_img),
+                        "tt_scf_img": url_for("static", filename=f_tt_img),
+
                         # plot
                         "conescfs_plot_json_in": conescfs_plot_json_in,
-                        "conescfs_plot_json_out": conescfs_plot_json_out
+                        "conescfs_plot_json_out": conescfs_plot_json_out,
+
         })
 
 
@@ -99,11 +157,44 @@ def cone_route():
     else:
         # Provide default values to render form
         default_data = {
+            "cone_junction": "large",
             "radius_tubular": 850,
             "thickness_tubular": 85,
             "thickness_cone": 37,
             "alpha": 1.72,
-            "cone_junction": "large"
+            'weld_width': 75,
+            'scf_taper_ratio': 4,
+            'delta_m': 5,
+            'delta_0': 5,
+            "transition_side": "inside",
+            # images
+            "cone_scf_img": url_for("static", filename="conescfs_tts/cone_large_junction.png"),
+            "tt_scf_img": url_for("static", filename="conescfs_tts/fig3_12c_and_12d.png"),
         }
 
         return render_template("conescfs.html", **default_data)
+
+
+def get_cone_and_tt_imgs(junction_type, transition_side, scf_weld_type, thickness_transition):
+
+    # get correct cone image
+    if junction_type == "small":
+        f_cone_img = "conescfs_tts/cone_small_junction.png"
+    else:
+        f_cone_img = "conescfs_tts/cone_large_junction.png"
+
+    # now thkness transition
+    if np.isclose(thickness_transition, 0.):
+        f_tt_img = "conescfs_tts/fig3_8.png"
+    elif transition_side == "outside" and scf_weld_type == "single_sided":
+        f_tt_img = "conescfs_tts/fig3_12a_and_12b.png"
+    elif transition_side == "inside" and scf_weld_type == "single_sided":
+        f_tt_img = "conescfs_tts/fig3_12c_and_12d.png"
+    elif transition_side == "outside" and scf_weld_type == "double_sided":
+        f_tt_img = "conescfs_tts/fig3_11_b.png"
+    elif transition_side == "inside" and scf_weld_type == "double_sided":
+        f_tt_img = "conescfs_tts/fig3_11_b_edited_tt_on_inside.png"  # this is a monopile usually :-)
+    else:
+        f_tt_img = "conescfs_tts/tt_todo.png"  # what image for this??!?
+
+    return f_cone_img, f_tt_img
--- conescfs/thktransitionscfs.py
+++ conescfs/thktransitionscfs.py
@@ -1,5 +1,4 @@
 import numpy as np
-import math
 from typing import Literal
 
 
@@ -8,11 +7,10 @@ def calc_scf_thickness_transition(
     diameter_thin_member: float,
     thickness_thick_member: float,
     thickness_thin_member: float,
-    weld_width_inner: float,
-    weld_width_outer: float,
-    max_misal: float,
-    builtin_misal_inner: float,
-    builtin_misal_outer: float,
+    weld_width: float,
+    delta_m: float,
+    delta_0: float,
+    taper_ratio: float, # e.g. 4 is in 1 in 4
     weld_type: Literal["single", "double"],  # single or double sided weld
     transition: Literal["inside", "outside"],  # transition on inner or outer side of plate
 ):
@@ -23,9 +21,8 @@ def calc_scf_thickness_transition(
         diameter_thin_member (float): outer diameter of thin member at weld location [m]
         thickness_thick_member (float): thickness of thick member at weld location [m]
         thickness_thin_member (float): thickness of thin member at weld location [m]
-        weld_width_inner (float): weld width, inner [m]
-        weld_width_outer (float): weld width, outer [m]
-        max_misal (float): maximum misalignment of the members [m]
+        weld_width (float): weld width [m]
+        delta_m (float): maximum misalignment of the members [m]
         builtin_misal_inner (float): built in misalignment of the members, inner side [m]
         builtin_misal_outer (float): built in misalignment of the members, outer side [m]
         weld_type (str, optional): specification of "single" or "double" sided weld. Defaults to "double".
@@ -34,47 +31,55 @@ def calc_scf_thickness_transition(
         SCFMember: SCF outer side, SCF inner side
     """
 
-    # Define centre diameter
-    centre_diameter_thick_member = (diameter_thick_member - thickness_thick_member)
+    # calculate centre line diameters
+    centre_diameter_thick_member = diameter_thick_member - thickness_thick_member
     centre_diameter_thin_member = diameter_thin_member - thickness_thin_member
 
-    # Define inner diameter
-    # inner_diameter_thick_member = (diameter_thick_member - 2 * thickness_thick_member)
-    # inner_diameter_thin_member = (diameter_thin_member - 2 * thickness_thin_member)
-
     # Transition thickness delta t
-    dt = np.abs(0.5 * ((centre_diameter_thick_member) - (centre_diameter_thin_member)))
+    dt = np.abs(0.5 * (centre_diameter_thick_member - centre_diameter_thin_member))
+    if weld_type == "single_sided":
+        delta_0 = 0.
+        print("delta_0 set to 0 as single sided weld")  # todo check this
 
-    if weld_type == "single":
-        builtin_misal_inner = 0
+    # determine whether weld width is either 1) the weld width or 2) the length of the tapered region
+    thickness_transition = thickness_thick_member - thickness_thin_member
+    length = taper_ratio * (thickness_transition) if taper_ratio is not None and thickness_transition != 0 else weld_width
+    # print(f"length {length}, taper ratio {taper_ratio}")
 
-    length_inner, length_outer = 999, 999 ### todo weld length L
-
-    if transition == "inside" or transition is None:
-        t_scf1 = dt + max_misal - builtin_misal_inner
-        t_scf2 = dt - max_misal + builtin_misal_outer
-    elif transition == "outside":
-        t_scf1 = dt + max_misal - builtin_misal_outer
-        t_scf2 = dt - max_misal + builtin_misal_inner
+    t_scf1 = dt + delta_m - delta_0
+    t_scf2 = dt - delta_m + delta_0
 
     # calculate constants
     beta = (1.5 - 1 / np.log10(diameter_thin_member / thickness_thin_member) + 3 / (np.log10(diameter_thin_member / thickness_thin_member)) ** 2)
-    alpha_inner = (1.82 * length_inner / np.sqrt(diameter_thin_member * thickness_thin_member) / (1 + (thickness_thick_member / thickness_thin_member) ** beta))
-    alpha_outer = (1.82 * length_outer / np.sqrt(diameter_thin_member * thickness_thin_member) / (1 + (thickness_thick_member / thickness_thin_member) ** beta))
+    alpha = (1.82 * length / np.sqrt(diameter_thin_member * thickness_thin_member) / (1 + (thickness_thick_member / thickness_thin_member) ** beta))
 
-    # Eq 3.3.5
+    # Eq 3.10
     scf1_c = (6 * t_scf1 / thickness_thin_member / (1 + (thickness_thick_member / thickness_thin_member) ** beta))
-    # Eq 3.3.6
+    # Eq 3.11
     scf2_c = (6 * t_scf2 / thickness_thin_member / (1 + (thickness_thick_member / thickness_thin_member) ** beta))
 
     if transition is None:
-        SCF_inside = 1 + scf1_c * np.exp(-alpha_inner)
-        SCF_outside = 1 + scf1_c * np.exp(-alpha_inner)
+        scf_inside = 1 + scf1_c * np.exp(-alpha) # Eq 3.10
+        scf_outside = 1 + scf1_c * np.exp(-alpha) # Eq 3.10
     elif transition == "inside":
-        SCF_inside = 1 + scf1_c * np.exp(-alpha_inner)
-        SCF_outside = 1 - scf2_c * np.exp(-alpha_outer)
-    else:
-        SCF_inside = 1 - scf2_c * np.exp(-alpha_inner)
-        SCF_outside = 1 + scf1_c * np.exp(-alpha_outer)
+        scf_inside = 1 + scf1_c * np.exp(-alpha) # Eq 3.10
+        scf_outside = 1 - scf2_c * np.exp(-alpha) # Eq 3.11
+    elif transition == "outside":
+        scf_inside = 1 - scf2_c * np.exp(-alpha)  # Eq 3.11
+        scf_outside = 1 + scf1_c * np.exp(-alpha)  # Eq 3.10
 
-    return SCF_inside, SCF_outside
+    # SCF for butt welds between members with equal thickness, 3.3.7.2 in DNV RP C203 2025
+    if np.isclose(thickness_transition, 0.):
+        print("SCF calculated for butt welds between members with equal thickness, 3.3.7.2 in DNV RP C203 2025. Taper ratio is ignored.")
+        alpha = 0.91 * weld_width / np.sqrt(diameter_thin_member * thickness_thin_member)
+        if weld_type == "single_sided":
+            # weld root (inside for single sided) SCF can be put to 1.0 as per text in C203 above fig 3-8
+            scf_inside = 1 #  - 3 * (delta_m - delta_0) * np.exp(-alpha) / thickness_thin_member
+            scf_outside = 1 + 3 * (delta_m - delta_0) * np.exp(-alpha) / thickness_thin_member
+        elif weld_type == "double_sided":
+            scf_inside = 1 + 3 * (delta_m - delta_0) * np.exp(-alpha) / thickness_thin_member
+            scf_outside = 1 + 3 * (delta_m - delta_0) * np.exp(-alpha) / thickness_thin_member
+
+
+
+    return scf_inside, scf_outside
