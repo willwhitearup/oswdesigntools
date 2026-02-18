262993d7b8b93b88e03d78c5f6afdffa28ce92ab
Will White <Will.White@arup.com>
2026-02-18 15:27:07 +0000

    updated now cone stuff

--- conescfs/cone_route.py
+++ conescfs/cone_route.py
@@ -1,8 +1,10 @@
+import copy
+
 import numpy as np
 from flask import Flask, request, jsonify, render_template, url_for
 # local
 from conescfs.coneplotter import cone_scfs_plot
-from conescfs.scfprocess import cone_scf_single, cone_scf_sweep
+from conescfs.scfprocess import cone_scf_single, cone_scf_sweep, tt_scf_process, cone_tt_scf_process
 from conescfs.thktransitionscfs import calc_scf_thickness_transition
 
 app = Flask(__name__)
@@ -41,20 +43,34 @@ def cone_route():
         scf_taper_ratio = float(data["scf_taper_ratio"]) if data.get("scf_taper_ratio") in (3., 4., 5., 6.) else None
         delta_m = data.get('delta_m')
         delta_0 = data.get('delta_0')
-        scf_inclusion = data.get('scf_inclusion')
+        scf_inclusion = data.get('scf_inclusion')  # "yes_multiply", "yes_linear_add", "no"
+        # print(data)
 
-        cone_scf_numeric_fields = {
-            "radius_tubular": radius_tubular,
-            "thickness_tubular": thickness_tubular,
-            "thickness_cone": thickness_cone,
-            "alpha": alpha
-        }
-
-        base_val = cone_scf_numeric_fields[cone_x_axis_vary]
-        x_arr = np.linspace(base_val * 0.5, base_val * 1.5, 21)
+        # add the images to the site based on inputs--------------------------------------------------------------------
+        f_cone_img, f_tt_img = get_cone_and_tt_imgs(junction_type, transition_side, scf_weld_type,
+                                                    thickness_transition=abs(thickness_cone-thickness_tubular))
 
         # cone SCF processing-------------------------------------------------------------------------------------------
-        single_results = cone_scf_single(radius_tubular, thickness_tubular, thickness_cone, alpha, junction_type)
+        # using the minimum thickness of either the cone and tube, gives governing SCFs at the critical loc i.e.
+        # at small diameter junction, the outside SCFs are larger, and at large diameter junction, the inside SCFs are larger
+        use_minimum_t = True
+        if "yes" in scf_inclusion:
+            if use_minimum_t:
+                thickness_tubular_override = min(thickness_tubular, thickness_cone)
+                thickness_cone_override = min(thickness_tubular, thickness_cone)
+            else:
+                thickness_tubular_override = max(thickness_tubular, thickness_cone)
+                thickness_cone_override = max(thickness_tubular, thickness_cone)
+        else:
+            thickness_tubular_override, thickness_cone_override = thickness_tubular, thickness_cone
+
+        cone_scf_numeric_fields = {"radius_tubular": radius_tubular, "thickness_tubular": thickness_tubular_override,
+            "thickness_cone": thickness_cone_override, "alpha": alpha}
+
+        base_val = cone_scf_numeric_fields[cone_x_axis_vary]
+        x_arr = np.linspace(base_val * 0.5, base_val * 1.5, 21)  # +/- 50% of the nominal value
+
+        single_results = cone_scf_single(radius_tubular, thickness_tubular_override, thickness_cone_override, alpha, junction_type)
         sweep_results = cone_scf_sweep(junction_type, cone_x_axis_vary, x_arr, cone_scf_numeric_fields)
 
         # single (one off) SCF results---------------------
@@ -83,52 +99,41 @@ def cone_route():
         scf_tube_out_appfs = sweep_results["appf17"]["tube_out"]
         scf_cone_out_appfs = sweep_results["appf17"]["cone_out"]
 
-        # plot the inside SCFs (outside and inside curves)
+        # thickness transition SCF processing--------------------------------------------------------------------------
+        # scfs inside and outside
+        scf_inside_tt, scf_outside_tt = tt_scf_process(thickness_tubular, thickness_cone, radius_tubular,
+                                                       transition_side, weld_width, delta_m, delta_0, scf_taper_ratio,
+                                                       scf_weld_type)
+
+        # Cone and thickness transition SCFs----------------------------------------------------------------------------
+        cone_tt_single_results = cone_tt_scf_process(single_results, scf_inside_tt, scf_outside_tt, scf_inclusion)
+        scf_tube_in_tt = cone_tt_single_results["sect3"]["tube_in_tt"]
+        scf_cone_in_tt = cone_tt_single_results["sect3"]["cone_in_tt"]
+        scf_tube_out_tt = cone_tt_single_results["sect3"]["tube_out_tt"]
+        scf_cone_out_tt = cone_tt_single_results["sect3"]["cone_out_tt"]
+
+        # --- App F ---
+        scf_tube_in_appf_tt = cone_tt_single_results["appf17"]["tube_in_tt"]
+        scf_cone_in_appf_tt = cone_tt_single_results["appf17"]["cone_in_tt"]
+        scf_tube_out_appf_tt = cone_tt_single_results["appf17"]["tube_out_tt"]
+        scf_cone_out_appf_tt = cone_tt_single_results["appf17"]["cone_out_tt"]
+
+        # plot the inside SCFs (outside and inside curves)---------------------------------------------------------------
         conescfs_plot_json_in = cone_scfs_plot(x_arr.tolist(), scf_tube_ins, scf_tube_in_appfs, scf_cone_ins, scf_cone_in_appfs, junction_type, cone_x_axis_vary, "INSIDE")
         conescfs_plot_json_out = cone_scfs_plot(x_arr.tolist(), scf_tube_outs, scf_tube_out_appfs, scf_cone_outs, scf_cone_out_appfs, junction_type, cone_x_axis_vary, "OUTSIDE")
 
-        # thickness transition SCF processing--------------------------------------------------------------------------
-        # scfs inside and outside
-        thk_diff = abs(thickness_tubular - thickness_cone)
+        # scf messages re. implementation approach
+        scf_msgs = get_scf_implementation_msgs(scf_inclusion, thickness_tubular_override)
 
-        # thicker tube than cone
-        if thickness_tubular >= thickness_cone:
-
-            thickness_thick_member, thickness_thin_member = thickness_tubular, thickness_cone
-
-            if transition_side == "inside":  # i.e. outer diameter is flush
-                diameter_thick_member = radius_tubular * 2
-                diameter_thin_member = radius_tubular * 2
-            elif transition_side == "outside":
-                diameter_thick_member = radius_tubular * 2
-                diameter_thin_member = radius_tubular * 2 - 2 * thk_diff
-
-
-        # thicker cone than tube
-        elif thickness_cone > thickness_tubular:
-
-            thickness_thick_member, thickness_thin_member = thickness_cone, thickness_tubular
-
-            if transition_side == "inside":  # i.e. outer diameter is flush
-                diameter_thick_member = radius_tubular * 2
-                diameter_thin_member = radius_tubular * 2
-
-            elif transition_side == "outside":
-                diameter_thick_member = radius_tubular * 2 + 2 * thk_diff
-                diameter_thin_member = radius_tubular * 2
-
-        # calculate weld width
-        scf_inside_tt, scf_outside_tt = calc_scf_thickness_transition(diameter_thick_member, diameter_thin_member,
-                                               thickness_thick_member, thickness_thin_member, weld_width,
-                                               delta_m, delta_0, scf_taper_ratio,  scf_weld_type, transition_side)
-
-
-        # images
-        f_cone_img, f_tt_img = get_cone_and_tt_imgs(junction_type, transition_side, scf_weld_type, thickness_transition=abs(thickness_cone-thickness_tubular))
 
         # Return results as JSON
         return jsonify({"status": "success",
+                        # images
+                        "cone_scf_img": url_for("static", filename=f_cone_img),
+                        "tt_scf_img": url_for("static", filename=f_tt_img),
                         "cone_junction": junction_type,
+                        "scf_inclusion": scf_inclusion,
+                        # Cone SCFs=======================================
                         # DNV section 3 SCFs
                         "scf_tube_in": scf_tube_in,
                         "scf_cone_in": scf_cone_in,
@@ -139,18 +144,26 @@ def cone_route():
                         "scf_cone_in_appf": scf_cone_in_appf,
                         "scf_tube_out_appf": scf_tube_out_appf,
                         "scf_cone_out_appf": scf_cone_out_appf,
-                        # thickness transition
+                        # thickness transition SCFs===========================
                         "scf_inside_tt":  scf_inside_tt,
                         "scf_outside_tt": scf_outside_tt,
-                        # images
-                        "cone_scf_img": url_for("static", filename=f_cone_img),
-                        "tt_scf_img": url_for("static", filename=f_tt_img),
-
-                        # plot
+                        # Cone SCFs including thickness transition SCFS
+                        # ===================================================
+                        # DNV section 3 SCFs
+                        "scf_tube_in_tt": scf_tube_in_tt,
+                        "scf_cone_in_tt": scf_cone_in_tt,
+                        "scf_tube_out_tt": scf_tube_out_tt,
+                        "scf_cone_out_tt": scf_cone_out_tt,
+                        # app F SCFs
+                        "scf_tube_in_appf_tt": scf_tube_in_appf_tt,
+                        "scf_cone_in_appf_tt": scf_cone_in_appf_tt,
+                        "scf_tube_out_appf_tt": scf_tube_out_appf_tt,
+                        "scf_cone_out_appf_tt": scf_cone_out_appf_tt,
+                        # plot============================================
                         "conescfs_plot_json_in": conescfs_plot_json_in,
                         "conescfs_plot_json_out": conescfs_plot_json_out,
-
-        })
+                        "scf_msgs": scf_msgs
+                        })
 
 
     # GET request
@@ -164,12 +177,12 @@ def cone_route():
             "alpha": 1.72,
             'weld_width': 75,
             'scf_taper_ratio': 4,
-            'delta_m': 5,
-            'delta_0': 5,
-            "transition_side": "inside",
+            'delta_m': 4,
+            'delta_0': 1.85,
+            "transition_side": "outside",
             # images
             "cone_scf_img": url_for("static", filename="conescfs_tts/cone_large_junction.png"),
-            "tt_scf_img": url_for("static", filename="conescfs_tts/fig3_12c_and_12d.png"),
+            "tt_scf_img": url_for("static", filename="conescfs_tts/fig3_12a_and_12b.png"),
         }
 
         return render_template("conescfs.html", **default_data)
@@ -198,3 +211,24 @@ def get_cone_and_tt_imgs(junction_type, transition_side, scf_weld_type, thicknes
         f_tt_img = "conescfs_tts/tt_todo.png"  # what image for this??!?
 
     return f_cone_img, f_tt_img
+
+def get_scf_implementation_msgs(scf_inclusion, thickness_tubular_override):
+    scf_msgs = {}
+    if scf_inclusion == "yes_multiply":
+        scf_msgs["1"] = "Cone SCFs and thickness transition SCFs are multiplied."
+    elif scf_inclusion == "yes_linear_add":
+        scf_msgs["1"] = "Cone SCFs and thickness transition SCFs are linearly summed (e.g. 1.2 + 1.4 = 1.6)."
+    else:
+        scf_msgs["1"] = "Cone SCFs are not combined with thickness transition SCFs."
+
+    if "yes" in scf_inclusion:
+        scf_msgs["2"] = (
+            f"Cone SCFs are calculated assuming a single thickness only; the minimum thickness of cone and tubular thicknesses ({thickness_tubular_override}) is used. "
+            f"This approach isolates the SCF to be from the bending induced by the cone angle only i.e. SCF from bending induced from centre line eccentricity between adjacent sections is NOT considered.")
+        scf_msgs["3"] = (
+            "Thickness transition SCFs consider both the geometric effect (sharp corner between 2 plates of different thickness) and the SCF from "
+            "bending induced from centre line eccentricity between adjacent sections.")
+        scf_msgs["4"] = ("This approach is used to avoid double counting the SCF induced from bending due to the "
+                         "centre line eccentricity i.e. this effect can be removed from the Cone SCF calculation but not be from the thickness transition SCF calculation.")
+
+    return scf_msgs
--- conescfs/scfprocess.py
+++ conescfs/scfprocess.py
@@ -1,4 +1,5 @@
 from conescfs.scfs import calc_cone_scfs_sect3, calc_cone_scfs_appf17
+from conescfs.thktransitionscfs import calc_scf_thickness_transition
 
 
 def cone_scf_single(radius_tubular, thickness_tubular, thickness_cone, alpha, junction_type):
@@ -15,25 +16,21 @@ def cone_scf_single(radius_tubular, thickness_tubular, thickness_cone, alpha, ju
 
 
 def cone_scf_sweep(junction_type, cone_x_axis_vary, x_arr, numeric_inputs):
-    """Returns SCFs over ±50% variation of selected variable.
+    """Returns SCFs over ±X% variation of selected variable.
     """
 
     keys = ["tube_in", "cone_in", "tube_out", "cone_out"]
-
-    results = {"x": x_arr, "sect3": {k: [] for k in keys}, "appf17": {k: [] for k in keys}
-               }
+    results = {"x": x_arr, "sect3": {k: [] for k in keys}, "appf17": {k: [] for k in keys}}
 
     for x in x_arr:
         args = numeric_inputs.copy()
         args[cone_x_axis_vary] = x
 
         sect3_vals = calc_cone_scfs_sect3(args["radius_tubular"], args["thickness_tubular"], args["thickness_cone"],
-            args["alpha"], junction_type
-        )
+            args["alpha"], junction_type)
 
         appf_vals = calc_cone_scfs_appf17(args["radius_tubular"], args["thickness_tubular"], args["thickness_cone"],
-            args["alpha"], junction_type
-        )
+            args["alpha"], junction_type)
 
         for key, val in zip(keys, sect3_vals):
             results["sect3"][key].append(val)
@@ -42,3 +39,73 @@ def cone_scf_sweep(junction_type, cone_x_axis_vary, x_arr, numeric_inputs):
             results["appf17"][key].append(val)
 
     return results
+
+def tt_scf_process(thickness_tubular, thickness_cone, radius_tubular, transition_side, weld_width, delta_m, delta_0,
+                   scf_taper_ratio, scf_weld_type):
+
+    thk_diff = abs(thickness_tubular - thickness_cone)
+
+    # thicker tube than cone
+    if thickness_tubular >= thickness_cone:
+
+        thickness_thick_member, thickness_thin_member = thickness_tubular, thickness_cone
+
+        if transition_side == "inside":  # i.e. outer diameter is flush
+            diameter_thick_member = radius_tubular * 2
+            diameter_thin_member = radius_tubular * 2
+        elif transition_side == "outside":
+            diameter_thick_member = radius_tubular * 2
+            diameter_thin_member = radius_tubular * 2 - 2 * thk_diff
+
+
+    # thicker cone than tube
+    elif thickness_cone > thickness_tubular:
+
+        thickness_thick_member, thickness_thin_member = thickness_cone, thickness_tubular
+
+        if transition_side == "inside":  # i.e. outer diameter is flush
+            diameter_thick_member = radius_tubular * 2
+            diameter_thin_member = radius_tubular * 2
+
+        elif transition_side == "outside":
+            diameter_thick_member = radius_tubular * 2 + 2 * thk_diff
+            diameter_thin_member = radius_tubular * 2
+
+    # calculate weld width
+    scf_inside_tt, scf_outside_tt = calc_scf_thickness_transition(diameter_thick_member, diameter_thin_member,
+                                                                  thickness_thick_member, thickness_thin_member, weld_width,
+                                                                  delta_m, delta_0, scf_taper_ratio, scf_weld_type,
+                                                                  transition_side)
+
+    return scf_inside_tt, scf_outside_tt
+
+
+def cone_tt_scf_process(single_results, scf_inside_tt, scf_outside_tt, scf_inclusion):
+
+    cone_tt_single_results = {}
+    for k, v in single_results.items():
+        cone_tt_single_results[k] = {}
+        for loc, cone_scf in v.items():
+            if loc.endswith("_in"):
+                if scf_inclusion == "yes_multiply":
+                    cone_tt_scf = cone_scf * scf_inside_tt
+                elif scf_inclusion == "yes_linear_add":
+                    cone_tt_scf = cone_scf + (scf_inside_tt - 1.)
+                else:
+                    cone_tt_scf = cone_scf
+
+            elif loc.endswith("_out"):
+                if scf_inclusion == "yes_multiply":
+                    cone_tt_scf = cone_scf * scf_outside_tt
+                elif scf_inclusion == "yes_linear_add":
+                    cone_tt_scf = cone_scf + (scf_outside_tt - 1.)
+                else:
+                    cone_tt_scf = cone_scf
+
+            else:
+                raise ValueError(f"Unexpected SCF key: {loc}")
+            cone_tt_single_results[k][f"{loc}_tt"] = cone_tt_scf
+
+    return cone_tt_single_results
+
+
